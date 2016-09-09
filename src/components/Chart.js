931b00ec7e29651111c7704c44b37289e6e07146
Yun <grape6@castis.com>
2016-09-09 11:01:01 +0900

    highchart 테마...임시

--- src/components/Chart.js
+++ src/components/Chart.js
@@ -52,6 +52,104 @@ class DonutChart extends React.Component {
     }
 
     componentDidMount() {
+        highcharts.createElement('link', {
+            href: 'https://fonts.googleapis.com/css?family=Signika:400,700',
+            rel: 'stylesheet',
+            type: 'text/css'
+        }, null, document.getElementsByTagName('head')[0]);
+
+// Add the background image to the container
+        highcharts.wrap(highcharts.Chart.prototype, 'getContainer', function (proceed) {
+            proceed.call(this);
+            this.container.style.background = 'url(http://www.highcharts.com/samples/graphics/sand.png)';
+        });
+
+
+        highcharts.theme = {
+            colors: ["#f45b5b", "#8085e9", "#8d4654", "#7798BF", "#aaeeee", "#ff0066", "#eeaaee",
+                "#55BF3B", "#DF5353", "#7798BF", "#aaeeee"],
+            chart: {
+                backgroundColor: null,
+                style: {
+                    fontFamily: "Signika, serif"
+                }
+            },
+            title: {
+                style: {
+                    color: 'black',
+                    fontSize: '16px',
+                    fontWeight: 'bold'
+                }
+            },
+            subtitle: {
+                style: {
+                    color: 'black'
+                }
+            },
+            tooltip: {
+                borderWidth: 0
+            },
+            legend: {
+                itemStyle: {
+                    fontWeight: 'bold',
+                    fontSize: '13px'
+                }
+            },
+            xAxis: {
+                labels: {
+                    style: {
+                        color: '#6e6e70'
+                    }
+                }
+            },
+            yAxis: {
+                labels: {
+                    style: {
+                        color: '#6e6e70'
+                    }
+                }
+            },
+            plotOptions: {
+                series: {
+                    shadow: true
+                },
+                candlestick: {
+                    lineColor: '#404048'
+                },
+                map: {
+                    shadow: false
+                }
+            },
+
+            // Highstock specific
+            navigator: {
+                xAxis: {
+                    gridLineColor: '#D0D0D8'
+                }
+            },
+            rangeSelector: {
+                buttonTheme: {
+                    fill: 'white',
+                    stroke: '#C0C0C8',
+                    'stroke-width': 1,
+                    states: {
+                        select: {
+                            fill: '#D0D0D8'
+                        }
+                    }
+                }
+            },
+            scrollbar: {
+                trackBorderColor: '#C0C0C8'
+            },
+
+            // General
+            background2: '#E0E0E8'
+
+        };
+
+        highcharts.setOptions(highcharts.theme);
+
         this.chart = $(ReactDOM.findDOMNode(this.refs.chart)).highcharts({
             chart: {
                 type: 'column'
@@ -70,24 +168,23 @@ class DonutChart extends React.Component {
             xAxis: [{
                 title: {
                     text: "channel_id"
-                }
+                },
+                type: 'category'
             }],
             plotOptions: {
-                pie: {
-                    shadow: false
+                series: {
+                    borderWidth: 0,
                 }
             },
             tooltip: {
-                formatter: function () {
-                    return '<b>' + this.point.name + '</b>: ' + this.y;
-                }
+                headerFormat: '<span style="font-size:11px">Sum of watch_time</span><br>',
+                pointFormat: '<span style="color:{point.color}">{point.name}</span>: <b>{point.y}</b><br/>'
             },
             series: [{
-                name: 'Browsers',
                 data: this.props.data,
                 size: '100%',
                 innerSize: '90%',
-                showInLegend: true,
+                showInLegend: false,
                 dataLabels: {
                     enabled: false
                 }
