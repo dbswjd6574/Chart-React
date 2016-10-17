d47299be0b3d3208632574bbf6f14af240d77b0e
salme4 <salme4@castis.com>
2016-10-17 14:14:48 +0900

    sunburst 차트 node별 이름 띄우기

--- src/components/SunburstChart.js
+++ src/components/SunburstChart.js
@@ -10,7 +10,44 @@ class SunburstChart extends React.Component{
         }
     }
     componentDidMount(){
+        var width = 700, height = 600, radius = Math.min(width, height) / 2;
+
+        var x = d3.scale.linear().range([0, 2 * Math.PI]);
+        var y = d3.scale.pow().exponent(1.3).domain([0, 1]).range([0, radius]);
+
+        var color = d3.scale.category20();
+
+        var svg = d3.select("#chart").append("svg")
+            .attr("width", width)
+            .attr("height", height)
+            .append("g")
+            .attr("transform", "translate(" + width / 2 + "," + (height / 2) + ")");
+
+        var partition = d3.layout.partition();
+
+        var arc = d3.svg.arc()
+            .startAngle(function(d) {
+                return Math.max(0, Math.min(2 * Math.PI, x(d.x)));
+            })
+            .endAngle(function(d) {
+                return Math.max(0, Math.min(2 * Math.PI, x(d.x + d.dx)));
+            })
+            .innerRadius(function(d) {
+                return Math.max(0, y(d.y));
+            })
+            .outerRadius(function(d) {
+                return Math.max(0, y(d.y + d.dy));
+            });
+
+        var tooltip = d3.select("#chart")
+            .append("div")
+            .attr("class", "tooltip")
+            .style("position", "absolute")
+            .style("z-index", "1")
+            .style("opacity", 0);
+
         d3.json('./sunburstData.json', function(error, root) {
+
             var path = svg.selectAll("path")
                 .data(partition.nodes(root))
                 .enter().append("path")
@@ -36,6 +73,15 @@ class SunburstChart extends React.Component{
                 .on("mouseout", function() {
                     return tooltip.style("opacity", 0);
                 });
+            var g = svg.selectAll("g").data(partition.nodes(root)).enter().append("g");
+            var text = g.append("text")
+                .attr("transform", function(d) { return "rotate(" + computeTextRotation(d) + ")"; })
+                .attr("x", function(d) { return y(d.y); })
+                .attr("dx", "6") // margin
+                .attr("dy", ".35em") // vertical-align
+                .attr("font-size", "11")
+                .text(function(d) { return d.name; });
+
 
             function zoom(d) {
                 path.transition()
@@ -43,6 +89,9 @@ class SunburstChart extends React.Component{
                     .attrTween("d", arcTween(d));
             }
 
+            function computeTextRotation(d) {
+                return (x(d.x + d.dx / 2) - Math.PI / 2) / Math.PI * 180;
+            }
             var checkedNode = [];
 
             function clickChart(d){
@@ -57,7 +106,7 @@ class SunburstChart extends React.Component{
 
                 d3.selectAll("path").style("opacity" , 0.2);
                 d3.selectAll("path").filter(function(node) {
-                        return (checkedNode.indexOf(node) >= 0);
+                    return (checkedNode.indexOf(node) >= 0);
                 }).style("opacity", 1);
             }
 
@@ -97,68 +146,7 @@ class SunburstChart extends React.Component{
                 return checkedNode;
 
             }
-/*
-
-            function getAncestors(checkedNode , node) {
-                var current = node;
-                if(checkedNode.indexOf(current) >= 0){
-
-                    console.log("exist current node : " + current.name);
-                    path.splice(checkedNode.indexOf(current), 1);
-                    uncheckChild(checkedNode , current);
-
-                }else {
-                    while (current.parent) {
-                        let index = checkedNode.indexOf(current);
-                        if(index == -1) {
-                            checkedNode.unshift(current);
-                        }
-                      current = current.parent;
-                    }
-                }
-                return checkedNode;
-            }
-*/
-
-
-
         });
-        var width = 700, height = 600, radius = Math.min(width, height) / 2;
-
-        var x = d3.scale.linear().range([0, 2 * Math.PI]);
-        var y = d3.scale.sqrt().range([0, radius]);
-
-        var color = d3.scale.category20();
-
-        var svg = d3.select("#chart").append("svg")
-            .attr("width", width)
-            .attr("height", height)
-            .append("g")
-            .attr("transform", "translate(" + width / 2 + "," + (height / 2) + ")");
-
-        var partition = d3.layout.partition();
-
-        var arc = d3.svg.arc()
-            .startAngle(function(d) {
-                return Math.max(0, Math.min(2 * Math.PI, x(d.x)));
-            })
-            .endAngle(function(d) {
-                return Math.max(0, Math.min(2 * Math.PI, x(d.x + d.dx)));
-            })
-            .innerRadius(function(d) {
-                return Math.max(0, y(d.y));
-            })
-            .outerRadius(function(d) {
-                return Math.max(0, y(d.y + d.dy));
-            });
-
-        var tooltip = d3.select("#chart")
-            .append("div")
-            .attr("class", "tooltip")
-            .style("position", "absolute")
-            .style("z-index", "1")
-            .style("opacity", 0);
-
         function arcTween(d) {
             var xd = d3.interpolate(x.domain(), [d.x, d.x + d.dx]),
                 yd = d3.interpolate(y.domain(), [d.y, 1]),
